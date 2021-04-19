1816e0d58e88b9d90494a3f58d42483c7e13081d
shravan20 <mrshravankumarb@gmail.com>
2021-04-19 15:26:56 +0530

    :sparkles: Updated .gitignore to remove the Resources

--- 002-Lambda_Streams/src/com/practice/interfaces/GreetingService.java
+++
@@ -1,5 +0,0 @@
-package com.practice.interfaces;
-
-public interface GreetingService {
-		void sayMessage(String message);
-}
--- 002-Lambda_Streams/src/com/practice/interfaces/MathOperation.java
+++
@@ -1,5 +0,0 @@
-package com.practice.interfaces;
-
-public interface MathOperation {
-	int operation(int n1, int n2);
-}

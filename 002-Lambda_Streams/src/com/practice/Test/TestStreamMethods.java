454d4510b223e3e653b70809741b2d5c0f7cf574
shravan20 <mrshravankumarb@gmail.com>
2021-04-19 15:28:47 +0530

    :sparkles: Minor changes in Lamdba|Streams programs

---
+++ 002-Lambda_Streams/src/com/practice/Test/TestStreamMethods.java
@@ -0,0 +1,40 @@
+package com.practice.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class TestStreamMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers = Arrays.asList(2,3,4,5,6,7);
+		List<Integer> square = numbers.stream().map(x -> x*x).collect(Collectors.toList());
+		System.out.println("Squares::"+square);
+		
+		
+	    List<String> names = Arrays.asList("Reflection","Collection","Stream");
+		List<String> result = names.stream().filter(s->s.startsWith("S")).collect(Collectors.toList());
+		System.out.println("Result::"+result);
+		
+	  
+	    Set<Integer> squareSet = numbers.stream().map(x->x*x).collect(Collectors.toSet());
+	    System.out.println("Before SquareSet"+squareSet);
+	     
+	    
+	    System.out.println("After SquareSet"+numbers.stream().map(x->x*x));
+
+	    numbers.stream().map(x->x*x).forEach(y->System.out.println("ForEach - Numbers::"+y));
+	    
+	    int sum = Arrays.stream(new int[]{1, 2, 3})
+	    		  .filter(i -> i >= 2)
+	    		  .map(i -> i * 3)
+	    		  .sum();
+	    
+	    System.out.println("sum="+sum);
+	    
+	}
+
+}

ab70b52c70400ad0608ce7c7aceae32c66546dfa
shravan20 <mrshravankumarb@gmail.com>
2021-04-20 18:25:41 +0530

    :sparkles: Added Model
    Added UserInfo Model

--- 004-UserInfo-api-JPA-Hibernate/src/main/java/com/practice/models/UserInfo.java
+++ 004-UserInfo-api-JPA-Hibernate/src/main/java/com/practice/models/UserInfo.java
@@ -1,11 +1,64 @@
 package com.practice.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name="UserInfo")
+@Table(name="userinfo")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class UserInfo {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(name="fullname")
+	private String fullName;
+	
+	@Column(name="country")
+	private String country;
+		
+	public UserInfo(int id, String fullName, String country) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserInfo [id=" + id + ", fullName=" + fullName + ", country=" + country + "]";
+	}	
 	
 	
 }

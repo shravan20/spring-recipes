402035b9277e9d5abe640dab5e560ae567500db8
shravan20 <mrshravankumarb@gmail.com>
2021-04-21 18:57:14 +0530

    :sparkles: Base Setup for DAO+SERVICES+MODELS+CONTROLLER Implemented

---
+++ Assignment7-PersonCRUDApplication/src/main/java/com/practice/models/Person.java
@@ -0,0 +1,55 @@
+package com.practice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_person")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+		
+	private String country;
+	
+	private int age;
+
+	private String bloodGroup;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+		
+}

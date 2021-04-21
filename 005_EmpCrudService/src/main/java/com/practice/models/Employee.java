891a85caead9b35102dfe99164e15651a24ffa5b
shravan20 <mrshravankumarb@gmail.com>
2021-04-21 18:20:59 +0530

    :sparkles: Added EmployeeModels

---
+++ 005_EmpCrudService/src/main/java/com/practice/models/Employee.java
@@ -0,0 +1,49 @@
+package com.practice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id
+	private int id;
+	private String firstName;
+	private String lastName;
+	private double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ "]";
+	}
+	
+	
+	
+}

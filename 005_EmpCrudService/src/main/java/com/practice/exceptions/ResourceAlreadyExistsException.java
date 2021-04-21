a5a4ce3ecdb869b28538afefa3d18d03bbf83095
shravan20 <mrshravankumarb@gmail.com>
2021-04-21 18:19:52 +0530

    :sparkles: Added Global Error handlers

---
+++ 005_EmpCrudService/src/main/java/com/practice/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.practice.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+
+	public ResourceAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}

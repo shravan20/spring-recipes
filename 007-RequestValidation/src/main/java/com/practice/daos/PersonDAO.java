dedad34ea5f5d100243decbb2a1dda027683e760
shravan20 <mrshravankumarb@gmail.com>
2021-04-23 16:13:53 +0530

    :sparkles: Implementation of Request Validation
    Implemented Requestion Validation for Body, Query and Params using Mock-CRUD Example

---
+++ 007-RequestValidation/src/main/java/com/practice/daos/PersonDAO.java
@@ -0,0 +1,11 @@
+package com.practice.daos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.practice.models.Person;
+
+public interface PersonDAO extends CrudRepository<Person, Integer> {
+	
+	
+
+}

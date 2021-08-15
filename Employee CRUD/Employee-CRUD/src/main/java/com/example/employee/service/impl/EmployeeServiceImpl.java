4c6540899aa70669de3addc654d243f5a4cf9f82
shravan20 <mrshravankumarb@gmail.com>
2021-08-16 01:11:53 +0530

    :sparkles: ServiceImpl
    Emp ServiceImpl

--- Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/service/impl/EmployeeServiceImpl.java
+++ Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/service/impl/EmployeeServiceImpl.java
@@ -2,13 +2,25 @@ package com.example.employee.service.impl;
 
 import com.example.employee.dto.EmployeeRequestDto;
 import com.example.employee.dto.EmployeeResponseDto;
+import com.example.employee.entity.Employee;
+import com.example.employee.mapper.EmployeeMapper;
+import com.example.employee.repository.EmployeeRepository;
 import com.example.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+
     @Override
     public EmployeeResponseDto create(EmployeeRequestDto employee) {
-        return null;
+        Employee e = employeeRepository.save(employeeMapper.toEntity(employee));
+        return EmployeeResponseDto.builder()
+                .id(e.getId()).firstName(e.getFirstName()).lastName(e.getLastName())
+                .build();
     }
 }

090433e3becf7c0b58d6ab39a6ae49bbc7614a2d
shravan20 <mrshravankumarb@gmail.com>
2021-08-15 17:16:19 +0530

    :sparkles: Added new project
    Emp-CRUD-App

---
+++ Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/EmployeeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee.Employee.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+}
---
+++ Employee CRUD/Employee-CRUD/src/test/java/com/example/employee/Employee/CRUD/EmployeeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee.Employee.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

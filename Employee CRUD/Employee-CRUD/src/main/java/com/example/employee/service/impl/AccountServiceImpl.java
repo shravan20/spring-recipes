85f83da28797c6691dc797be35f86b409946f0ba
shravankb <shravan.kumar@nineleaps.com>
2021-08-29 00:54:14 +0530

    :art: Updated code
    updated code

--- Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/service/impl/AccountServiceImpl.java
+++ Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/service/impl/AccountServiceImpl.java
@@ -5,10 +5,11 @@ import com.example.employee.dto.AccountRequestDto;
 import com.example.employee.dto.AccountResponseDto;
 import com.example.employee.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AccountServiceImpl implements AccountService {
     @Override
     public AccountResponseDto create(AccountRequestDto accountRequest) {

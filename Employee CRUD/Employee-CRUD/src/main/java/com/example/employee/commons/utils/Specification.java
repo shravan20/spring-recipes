e04f7a6cb49784bcc0c73d4e7833de64e03bfc00
shravankb <shravan.kumar@nineleaps.com>
2021-08-29 19:17:27 +0530

    :art: Added Specs | Folder restructuring
    Added Specs | Folder restructuring

--- Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/commons/utils/Specification.java
+++ Employee CRUD/Employee-CRUD/src/main/java/com/example/employee/commons/utils/Specification.java
@@ -1,16 +1,15 @@
+/* (C)2021 */
 package com.example.employee.commons.utils;
 
+import java.io.Serializable;
+import java.util.function.Predicate;
 import javax.annotation.Nullable;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import java.io.Serializable;
-import java.util.function.Predicate;
 
 public interface Specification<T> extends Serializable {
 
     @Nullable
-    Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
-                          CriteriaBuilder criteriaBuilder);
-    
+    Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder);
 }

77f8cf39a47081fb7a7a43f40556d8c0cd985c78
shravan20 <mrshravankumarb@gmail.com>
2021-08-31 00:41:23 +0530

    :construction: Added constant util file
    Added constant util file

---
+++ moodle-spring-boot-integration/src/main/java/com/moodle/integration/commons/util/Constant.java
@@ -0,0 +1,12 @@
+package com.moodle.integration.commons.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+    public static final String SUCCESS = "SUCCESS";
+
+    public static final String ERROR = "ERROR";
+
+    public static final String SIMULATOR_API = "/api/moodle-service/v1";
+}

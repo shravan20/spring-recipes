b79f40bd983a1e12af06fa65d061d848855e2cf7
shravan20 <mrshravankumarb@gmail.com>
2021-09-16 06:16:01 +0530

    :art: Restructured the code
    Restructured the code

--- microservice-projects/Employee-CRUD/src/main/java/com/example/EmployeeApplication.java
+++ microservice-projects/Employee-CRUD/src/main/java/com/example/EmployeeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeCrudApplication {
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeCrudApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
 }
--- microservice-projects/Employee-CRUD/src/test/java/com/example/employee/Employee/CRUD/EmployeeApplicationTests.java
+++ microservice-projects/Employee-CRUD/src/test/java/com/example/employee/Employee/CRUD/EmployeeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeCrudApplicationTests {
+class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {
